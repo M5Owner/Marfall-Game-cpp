428b39214a2133bbdddec3674c4b170810e5b578
Ali Lachguer <alikarita@hotmail.fr>
2015-12-10 17:20:51 +0100

    implementation

---
+++ MarFall/input.h
@@ -0,0 +1,40 @@
+
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <SFML/Graphics.hpp>
+
+
+class Input
+{
+
+	//Structures
+	struct Button { bool left, right, up, down, jump, attack, enter; };
+
+public:
+
+	//Constructeur
+	Input::Input();
+
+	//Accesseur
+	Button Input::getButton(void) const;
+
+	//Mutateur
+	void Input::setButton(int bouton, bool etat);
+
+	//Fonctions
+	void Input::gestionInputs(sf::RenderWindow &window);
+	void Input::getInput(sf::RenderWindow &window);
+
+
+private:
+
+	//Variables de la classe en accès privé
+	sf::Event event;
+	Button button;
+
+	//Enum pour les boutons
+	const enum{ up, down, right, left, attack, jump, enter };
+
+};
+#endif
---
+++ MarFall/main.h
@@ -0,0 +1,16 @@
+
+
+#include <cstdlib>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+#include "input.h"
+#include "map.h"
+#include "player.h"
+using namespace std;
+using namespace sf;
+
+// Taille de la fenêtre : 800x480 pixels 
+const int  SCREEN_WIDTH = 800;
+const int  SCREEN_HEIGHT = 480;
+	
